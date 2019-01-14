c8bb8b8f4e44847bdf0804b35c164e719eb4a91c
goyal_parth <2017csb1095@iitrpr.ac.in>
2019-01-14 23:07:08 +0530

    Preprocessing Template added

---
+++ 1. Preprocessing/Preprocessing.py
@@ -0,0 +1,55 @@
+"""
+@author: parthgoyal123
+"""
+
+''' ---------- Preprocessing ------------ '''
+
+# Importing the required libraries
+# ---> Numpy arrays are the most convinient way to work on Machine Learning models
+# ---> Matplotlib allows us to visualise our model in form of various plots/figures
+# ---> Pandas allows us to import the dataset efficiently
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset using pandas library
+# ---> pandas create a dataframe of the dataset
+# ---> iloc : It locates the column by its index. In other words, using ’iloc’ allows us to take columns by just taking their index.
+# ---> .values : It returns the values of the column (by their index) inside a Numpy array(way more efficient than a list)
+dataset = pd.read_csv("Filename.csv")
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Fixing the missing values from the dataset using sklearn.impute
+# ---> Importing the SimpleImputer class from the sklearn.impute library
+# ---> .fit : The fit part is used to extract some info of the data on which the object is applied
+# ---> .transform : the transform part is used to apply some transformation
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+X[:, [1,2]] = imputer.fit_transform(X[:, [1,2]])
+
+# Encoding categorical data (optional part, depends on the dataset)
+# ---> LabelEncoder encodes the categorical data to [0, n_values]
+# ---> OneHotEncoder seperates the LabelEncoded values to different columns, appended leftmost of the dataset
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_x = LabelEncoder()
+labelencoder_y = LabelEncoder()
+onehotencoder = OneHotEncoder(categorical_features= [0])
+X[:, 0] = labelencoder_x.fit_transform(X[:, 0])
+y = labelencoder_y.fit_transform(y)
+X = onehotencoder.fit_transform(X).toarray()
+
+# Dividing the dataset to training and test set
+# ---> train_test_split : function of the sklearn.model_selection library that splits the dataset to training and test sets
+# ---> random_state : this parameter is essential to get the same training and test splits(when improving the model time by time)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+
+# Feature Scaling the data(if required)
+# ---> StandardScaler : no parameters are to be passed to this class, only an object is to be made
+from sklearn.preprocessing import StandardScaler
+scale = StandardScaler()
+X_train = scale.fit_transform(X_train)
+X_test = scale.fit_transform(X_test)
+
+# ================ Preprocessing Complete ================== #
